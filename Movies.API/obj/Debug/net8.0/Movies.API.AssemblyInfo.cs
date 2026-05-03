b717252993f8830ce67bdf51022986f6d674c111
fatemeh-imani <fatemehimani.net@gmail.com>
2026-05-03 19:21:42 +0330

    the first API Project Nick

--- Movies.API/obj/Debug/net8.0/Movies.API.AssemblyInfo.cs
+++ Movies.API/obj/Debug/net8.0/Movies.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eee7d3d7bf36c515906d09e08c76d590fee3649")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
